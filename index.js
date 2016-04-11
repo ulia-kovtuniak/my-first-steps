a16a7d4d2e372a12b1156083d7c7402a2ed41b1a
ulia_kovtuniak <uliakovtuniak@gmail.com>
2016-04-11 12:54:16 +0000

    Initial commit

--- index.js
+++ index.js
@@ -6,4 +6,21 @@ function openNav(navName) {
         x[i].style.display = "none";
     }
     document.getElementById(navName).style.display = "block";
-}
+};
+$(function(){
+    var em = "uliakovtuniak";
+    em +="@";
+    em +="gmail.com";
+    $('#bt').click(function() {
+        $.ajax({
+            url:"https://formspree.io/" + em,
+            method: "POST",
+            data: {
+                Name: name.value,
+                Email: email.value,
+                Message: msg.value
+            },
+            dataType: 'json'
+        });
+    });
+});
